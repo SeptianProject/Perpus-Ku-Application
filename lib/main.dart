56703d45b6d8a4e54986d823d8df6c701bfefa1b
Septian Asropik <septianuwoe75@gmail.com>
2024-08-05 11:05:44 +0700

    Reusable Widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:perpus_digital/views/onboard/onboarding_page.dart';
 import 'package:perpus_digital/views/onboard/signin_page.dart';
 import 'package:perpus_digital/views/splash/splash_screen.dart';
 import 'firebase_options.dart';
